3e27bbd89de57c2d690a15d9e5a9dafaf9b0b239
Manan kumar pal <manangamer321@gmail.com>
2023-06-20 01:17:55 +0530

    done

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const productRouter = require("./routes/product");
 const userRouter = require("./routes/user");
 
 //INIT
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const DB =
   "mongodb+srv://manankumar1702:Manan%402003@cluster0.oplihuv.mongodb.net/?retryWrites=true&w=majority";
